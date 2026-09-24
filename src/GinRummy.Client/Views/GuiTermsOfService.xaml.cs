29eb7a32b18c714c71dc1847acc7178e17fccd97
Isaac <zS24013258@estudiantes.uv.mx>
2026-09-23 22:16:26 -0600

    feat(gui): add the terms of service screen (P24)

---
+++ src/GinRummy.Client/Views/GuiTermsOfService.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace GinRummy.Client.Views
+{
+    /// <summary>
+    /// Terms of service screen (P24). Opened from the banned account screen of CU-02 EX-05.
+    /// </summary>
+    public partial class GuiTermsOfService : GuiWindowBase
+    {
+        /// <summary>
+        /// Builds the terms of service screen.
+        /// </summary>
+        public GuiTermsOfService()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCloseClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
